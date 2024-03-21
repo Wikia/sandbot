fe99601486f33c64d225ef98642488703958b6a7
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2024-03-21 23:15:21 +0100

    update AdEng slack ID in the channels.js

--- db/015_update_adeng_channel_slack_id.js
+++ db/015_update_adeng_channel_slack_id.js
@@ -2,7 +2,7 @@ const db = require('./connection');
 const channels = require('../channels');
 
 db.serialize(() => {
-  db.run(`UPDATE sandboxes SET team = 'C0GV00TC4' WHERE team = 'G0GV00TC4';`);
+  db.run(`UPDATE sandboxes SET team = '${channels.ADENG_CHANNEL_ID}' WHERE team = 'G0GV00TC4';`);
 
   console.log('AdEng slack ID has been updated.');
 
