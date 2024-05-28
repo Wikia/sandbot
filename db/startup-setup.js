31d21f0be35d9abbbf5bc2e255ae54bc89fa1397
Weronika Dąbrowska <wdabrowska@fandom.com>
2024-05-28 13:33:25 +0200

    Feat: Initialise DB during startup from numbered files in /db directory

---
+++ db/startup-setup.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+
+async function setup() {
+	fs.readdir(`${__dirname}`, async (err, files) => {
+		if (err) {
+			console.error('Error reading directory:', err);
+			return;
+		}
+
+		let toProcess = [];
+
+		files.forEach((file) => {
+			//console.log(file);
+			if (/[\d]{3}.+\.js/.test(file)) {
+				toProcess.push(file);
+			}
+		});
+
+		const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+		const sorted = toProcess.sort((a, b) => collator.compare(a, b));
+		await sorted.forEach((file) => {
+			console.log(`==> Processing file: ${file}`);
+			const stdOut = execSync(`node ${file}`);
+			console.log(stdOut.toString());
+		});
+
+	});
+}
+
+setup();
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 . /var/lib/secrets/export.env
 export SANDBOT_TOKEN=$SECRET_APP_SANDBOT_DEV______________
+cd db && node startup-setup.js && cd ..
 npm run bot
 exec "$@"
