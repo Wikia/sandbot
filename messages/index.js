a3770b0f523877536ab837c03b59ddc11c612073
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2025-04-05 11:13:49 +0200

    Migrate remaining actions

--- messages/index.js
+++ messages/index.js
@@ -1,5 +1,13 @@
+const book = require('./book');
+const joke = require('./joke');
 const ping = require('./ping');
+const release = require('./release');
+const status = require('./status');
 
 module.exports = [
+  book,
+  joke,
   ping,
+  release,
+  status,
 ];
