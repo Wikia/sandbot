2e7f2289c9a93168ea88ada84a0240361bb1afd0
Weronika Dąbrowska <wdabrowska@fandom.com>
2025-04-07 14:34:50 +0200

    Fix, remove exports whatsoever (change in yaml should do the trick)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env/ bash
 set -e
 . /var/lib/secrets/export.env
-printenv
-export SLACK_APP_TOKEN=$SECRET_APP_SANDBOT_DEV______________
-export SLACK_BOT_TOKEN=$SECRET_APP_SANDBOT_DEV______________
-export SLACK_SIGNING_SECRET=$SECRET_APP_SANDBOT_DEV______________
 cd db && node startup-setup.js && cd ..
 npm run bot
 exec "$@"
