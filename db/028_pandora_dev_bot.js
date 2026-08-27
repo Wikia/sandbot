cfa52193e7d7979e6d2de73a8c3a6eed2a89d8c9
Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>
2026-08-27 14:36:28 +0200

    Update sandbox entries for Pandora dev channel

--- db/028_pandora_dev_bot.js
+++ db/028_pandora_dev_bot.js
@@ -2,8 +2,8 @@ const db = require('./connection');
 const channels = require('../channels');
 
 db.serialize(() => {
-  db.run(`INSERT INTO sandboxes VALUES('social-platform-dev-pl', '${channels.SOCIAL_PLATFORM_CHANNEL_ID}', '', null);`);
-  db.run(`INSERT INTO sandboxes VALUES('social-platform-dev-sjc', '${channels.SOCIAL_PLATFORM_CHANNEL_ID}', '', null);`);
+  db.run(`INSERT INTO sandboxes VALUES('social-platform-dev-pl', '${channels.PANDORA_DEV_CHANNEL_ID}', '', null);`);
+  db.run(`INSERT INTO sandboxes VALUES('social-platform-dev-sjc', '${channels.PANDORA_DEV_CHANNEL_ID}', '', null);`);
 
   console.log('Social platform pandora dev env reservation.');
 
